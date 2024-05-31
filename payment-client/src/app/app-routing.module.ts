6df61818475c9dfbac7d4c051f86ca77830f6b8f
Sorin Giurgica <H3XM@it7.eurofins.com>
2024-05-31 18:09:21 +0200

    Improve app module

--- payment-client/src/app/app-routing.module.ts
+++ payment-client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PayComponent } from './pay/pay.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PayComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- payment-client/src/app/app.module.ts
+++ payment-client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PayComponent } from './pay/pay.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { PayComponent } from './pay/pay.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
